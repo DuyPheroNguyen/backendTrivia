6db3ca542744af90a8ac5c00a56f0254b4617dc8
Jannus_Dev <jan.honing2@gmail.com>
2025-12-07 17:38:45 +0100

    Add JSON page

---
+++ TriviaWebRazorPagesGroepsopdracht/Pages/JSON.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TriviaWebRazorPagesGroepsopdracht.Pages;
+
+public class JSON : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
